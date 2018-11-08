8ad5c6c25709197114ca370e894468ab1c534c5a
MindBreaker3310 <excalibur3310@gmail.com>
2018-11-08 23:41:26 +0800

    show message in html

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,35 @@
 var socket= io();
-
+//連線
 socket.on('connect',()=>{
   console.log('connect to server');
-  
-  socket.emit('createMessage',{
-    from:'max',
-    text:'connected',
-    createdAt:'2018'
-  });
-
 });
-
+//接收伺服器訊息
 socket.on('newMessage',(message)=>{
-  console.log(message);
+  console.log('newMessage',message);
+  //放到list裡
+  var li=jQuery('<li></li>');
+  li.text(`${message.from}:${message.text}`);
+  jQuery('#messages').append(li);
 });
-
+//伺服器關閉
 socket.on('disconnect',()=>{
   console.log('disconnect');
 });
+//傳送訊息至伺服器
+// socket.emit('createMessage',{
+//   from:'client',
+//   text:'say hi',
+// },(serverMessage)=>{
+//   console.log('callback got it',serverMessage);
+// });
+//
+jQuery('#message-form').on('submit',(e)=>{
+  e.preventDefault();
 
-socket.on('new email',(mail)=>{
-  console.log('new email',mail);
+  socket.emit('createMessage',{
+    from:'user1',
+    text:jQuery('[name=message-input]').val()
+  },(callback)=>{
+    console.log('callback done');
+  });
 });
--- server/server.js
+++ server/server.js
@@ -14,36 +14,34 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
   console.log('new user connected');
-
+//用戶歡迎訊息
   socket.emit('newMessage',{
     from:'Admin',
     text:'welcome to the chat app',
     createdAt:new Date().getTime()
   });
-
+//廣播新用戶加入
   socket.broadcast.emit('newMessage',{
     from:'Admin',
     text:'new user joined',
     createdAt:new Date().getTime()
   });
-
-  socket.on('createMessage',(message)=>{
+//接收客戶端創建的訊息
+  socket.on('createMessage',(message,callback)=>{
     console.log(message);
+    //發送訊息至所有用戶端
     io.emit('newMessage',{
       from: message.from,
       text: message.text,
       createdAt:new Date().getTime()
     });
+    callback('serverMessage callback');
   });
-
+//用戶離開
   socket.on('disconnect',()=>{
     console.log('user was disconnected');
   });
-
 });
-
-
-
 
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
